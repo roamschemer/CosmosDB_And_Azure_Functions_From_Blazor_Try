462db9a92d77f9cafb13f29f6c5af3a8aa7ca357
roamschemer <roamschemer@gmail.com>
2022-01-31 00:01:48 +0900

    idでの呼び出しと、idとjobでの呼び出しを作成

---
+++ Api/Persons/GetPersonFromId.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Persons {
+    public static class GetPersonFromId {
+        [FunctionName("GetPersonFromId")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "person/id/{id}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "gunshi-db", //Database id
+                collectionName: "persons", //Container id
+                ConnectionStringSetting = "CosmosDbConnectionString",
+                SqlQuery = "select * from users r where r.id = {id}"
+            )]IEnumerable<Person> persons,
+            ILogger log) {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(persons.First());
+        }
+    }
+}
---
+++ Api/Persons/GetPersonFromJobAndId.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Data;
+
+namespace Api.Persons {
+    public static class GetPersonFromJobAndId {
+        [FunctionName("GetPersonFromJobAndId")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "person/{partitionKey}/{id}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "gunshi-db", //Database id
+                collectionName: "persons", //Container id
+                ConnectionStringSetting = "CosmosDbConnectionString",
+                Id = "{id}",
+                PartitionKey = "{partitionKey}")] Person person,
+            ILogger log) {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(person);
+        }
+    }
+}
